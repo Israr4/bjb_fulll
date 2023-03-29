6d54ffa7f1a30b4cbe21be257b247fc4cfef8753
Israr4 <israrrajpoot34@gmail.com>
2023-03-29 12:18:45 +0500

    new file added

--- Push-Up-Logger-1/main.py
+++ Push-Up-Logger-1/main.py
@@ -1,5 +1,6 @@
 from website import create_app
 
+
 app = create_app()
 
 if __name__ == '__main__':
@@ -37,3 +38,90 @@ if __name__ == '__main__':
 
 
 # to create your own packages and send it to  your own servers.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# <form method = "POST" action = "/search"> class = "d-flex">
+# <input class = "form-control me-2" type = "search", name = "searched">
+# <buttom class = "btn btn-outline-secondary" type = "submit"> Search </button>
+# </form>
+
+
+# @auths.route('/search' , methods =["POST"])
+# def search():
+
+
+
+
+
+#     search.html    search ka bad yeh dat show ho ga       search by name, id, email kuch bi hoskta ha
+#     extends base.html
+#     <div class="table-responsive" >
+#                   <form method="GET"  action="/allclients" >
+#                   <table class="table table-hover" style="width: 700px;">
+#                     <thead class="thead-dark">
+#                       <tr>
+#                         <th scope="col"> <a href="{{url_for('view.all_clients')}}"> Id </a></th>
+#                         <th scope="col">Email</th>
+#                         <th scope="col">Full Name</th>
+#                       </tr>
+#                     </thead>
+#                     <tbody>
+                      
+#                       {% for i in students %}
+#                         <tr>
+#                           <td>{{ i.id}}</td>
+#                           <td>{{ i.email}}</td>
+#                           <td>{{ i.fullname}}</td>
+#                         </tr>
+#                       {% endfor %}
+                
+#                       </tbody>
+#                     </table>
+#                 </form>
+#     </div>
+
+
+# class searchForm(Form):
+#     UserName = StringField('Search course', validators=[DataRequired(), Length(max=60)])
+
+
+
+# @app.route('/search', methods=['GET', 'POST'])
+# def search():
+#     searchForm = searchForm()
+#     courses = models.User.query
+
+#     if searchForm.validate_on_submit():
+#         courses = courses.filter(models.User.fullname.like('%' + searchForm.UserName.data + '%'))
+
+#     courses = courses.order_by(models.User.fullname).all()
+
+#     return render_template('search.html', courses = courses)  
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# postgre addition of admin table
+# 
+# CREATE EXTENSION pgcrypto ;
+# SELECT gen_salt('md5') ;
+	
+# INSERT INTO admin_user(adminid, adminemail, adminpassword, adminfullname)
+# VALUES (1, 'admin1@gmail.com', crypt('123456789', gen_salt('md5')),'Israr');  
